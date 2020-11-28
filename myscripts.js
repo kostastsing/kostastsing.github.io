c2a62c691bb75fe307112cbdb7e851c15cbc2413
kostastsing <kostastsig.94@gmail.com>
2020-11-28 21:26:34 +0200

    Add media queries for small screens, mobile menu

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,17 @@
+const isMobile = window.matchMedia("only screen and (max-width: 850px)");
+isMobile.addListener(respondToMediaChange);
+respondToMediaChange(isMobile);
+
+function respondToMediaChange(isMobile){
+  if (!isMobile.matches){
+    if ($('.top-menu').hasClass('pressed')){
+      $('.top-menu').removeClass('pressed');
+      $('header').css({"height": "80px"});
+      $('.menu-toggle').removeClass('change');
+    }
+  }
+}
+
 $(document).ready(function() {
   var ctx = document.getElementById('radar-chart');
   var myChart = new Chart(ctx, {
@@ -49,3 +63,27 @@ $(document).ready(function() {
       }
     }
 })});
+
+$(document).ready(function() {
+  $('.menu-toggle').click(function () {
+    $('.top-menu').toggleClass('pressed');
+    if ($('.top-menu').hasClass('pressed')){
+      $('.menu-toggle').toggleClass('change');
+      $("header").css({"height": "350px"});
+    }
+    else {
+      $('.menu-toggle').toggleClass('change');
+      $("header").css({"height": "80px"});
+    }
+  });
+});
+
+$(document).ready(function() {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 80) {
+      $("header").css({"height": "80px"});
+      $('.menu-toggle').removeClass('change');
+      $('.top-menu').removeClass('pressed');
+    }
+  });
+});
