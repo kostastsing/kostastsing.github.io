1c8acbea1da72eae1b698cdefdea987a3d363184
kostastsing <kostastsig.94@gmail.com>
2020-11-30 15:40:11 +0200

    Add smooth scrolling js

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -6,6 +6,22 @@ $(document).ready(function() {
   $("#footer").load("theme/footer.html");
 });
 
+$(document).ready(function(){
+  $('.scroll').on('click',function(e){
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop:$($(this).attr('href')).offset().top + 'px'
+    },1000,'swing');
+});
+
+// if we have anchor on the url (calling from other page)
+if(window.location.hash){
+  // smooth scroll to the anchor id
+  $('html,body').animate({
+    scrollTop:$(window.location.hash).offset().top + 'px'},1000,'swing');
+  }
+});
+
 $(document).ready(function() {
   var ctx = document.getElementById('radar-chart');
   var myChart = new Chart(ctx, {
